86baf0bd111ffffc49f771d7205e3332a3cf5eef
Ayroid <ayushsk0000@gmail.com>
2024-03-09 09:50:11 +0530

    dbpopulate

--- Backend/dbPopulate.js
+++ Backend/dbPopulate.js
@@ -1,8 +1,5 @@
-import dotenv from "dotenv";
 import { USERMODEL } from "./model/userModel.js";
 import { Database } from "./config/database.js";
-
-dotenv.config();
 
 const MONGODB_URI = "ADD YOUR MONGODB URI HERE";
 
